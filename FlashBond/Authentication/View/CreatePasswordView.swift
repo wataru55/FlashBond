a49147f29c4a86084dcba417611629cb9856aaa1
Wataru Takahashi <wataru1355@icloud.com>
2024-09-09 11:52:11 +0900

    パスワードを入力する画面を作成

--- FlashBond/Authentication/View/CreatePasswordView.swift
+++ FlashBond/Authentication/View/CreatePasswordView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 
 struct CreatePasswordView: View {
+    @EnvironmentObject var viewModel: RegistrationViewModel
+    @Environment(\.dismiss) var dismiss
+
+    let _width = UIScreen.main.bounds.width
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack (spacing: 10) {
+            Text("パスワードの登録")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+
+            Text("６文字以上でパスワードを登録してください")
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+                .padding(.bottom)
+
+            TextField("ユーザーネーム", text: $viewModel.username)
+                .textInputAutocapitalization(.none)
+                .modifier(TextFieldModifier())
+
+            NavigationLink {
+                CompleteSignUpView()
+                    .environmentObject(viewModel)
+            } label: {
+                Text("次へ")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: _width - 30, height: 40)
+                    .background(LinearGradient(colors: [Color.indigo, Color.blue], startPoint: .leading, endPoint: .trailing))
+                    .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                    .padding(.top)
+            }
+            .shadow(color: .black.opacity(0.7), radius: 2, x: 0, y: 2)
+
+            Spacer()
+        } // VStack
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image(systemName: "chevron.left")
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
+    } // body
+} // CreatePasswordView
 
 #Preview {
     CreatePasswordView()
