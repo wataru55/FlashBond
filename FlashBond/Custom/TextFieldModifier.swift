1184f4524485b130bf72752ebdcda40585114dab
Wataru Takahashi <wataru1355@icloud.com>
2024-09-09 11:45:01 +0900

    add: テキストフィールドで共通して使用するカスタムモディファイアを作成

--- FlashBond/Custom/TextFieldModifier.swift
+++ FlashBond/Custom/TextFieldModifier.swift
@@ -5,4 +5,16 @@
 //  Created by  髙橋和 on 2024/09/09.
 //
 
-import Foundation
+import SwiftUI
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .textInputAutocapitalization(.none)
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .clipShape(RoundedRectangle(cornerRadius: 12.0))
+            .padding(.horizontal)
+    }
+}
