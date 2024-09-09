bcf7e79d4859d78fb469c15092c06d719b2f5dd1
Wataru Takahashi <wataru1355@icloud.com>
2024-09-09 10:01:10 +0900

    Initial Commit

---
+++ FlashBond/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashBond
+//
+//  Created by  髙橋和 on 2024/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashBond/FlashBondApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashBondApp.swift
+//  FlashBond
+//
+//  Created by  髙橋和 on 2024/09/09.
+//
+
+import SwiftUI
+
+@main
+struct FlashBondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
