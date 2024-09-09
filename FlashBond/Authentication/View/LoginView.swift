b5792c500e2cc73d12c177e1d880212f00f9b0de
Wataru Takahashi <wataru1355@icloud.com>
2024-09-09 11:48:11 +0900

    fix: ログイン画面を作成

--- FlashBond/Authentication/View/LoginView.swift
+++ FlashBond/Authentication/View/LoginView.swift
@@ -8,10 +8,76 @@
 import SwiftUI
 
 struct LoginView: View {
+
+    @StateObject private var viewModel = LoginViewModel()
+
+    let _width = UIScreen.main.bounds.width
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                // アプリのロゴ
+                //TODO: ロゴ画像を追加する
+                Text("FlashBond")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                    .padding(.bottom, 50)
+
+                // テキストフィールド
+                VStack (spacing: 15) {
+                    TextField("メールアドレス", text: $viewModel.email)
+                        .modifier(TextFieldModifier())
+
+                    SecureField("パスワード", text: $viewModel.password)
+                        .modifier(TextFieldModifier())
+
+                }
+
+                Button(action: {
+                    print("show forgot password")
+                }, label: {
+                    Text("パスワードを忘れた？")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                })
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.trailing)
+                .padding(.vertical, 5)
+
+                Button(action: {
+
+                }, label: {
+                    Text("ログイン")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .frame(width: _width - 30, height: 40)
+                        .background(LinearGradient(colors: [Color.indigo, Color.blue], startPoint: .leading, endPoint: .trailing))
+                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                        .padding(.top, 20)
+                })
+                .shadow(color: .black.opacity(0.7), radius: 2, x: 0, y: 2)
+
+                Spacer()
+
+                Divider()
+
+                NavigationLink {
+                    AddEmailView()
+                } label: {
+                    Text("アカウントの新規作成")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+                .padding(.vertical)
+            } // VStack
+        } // NavigationStack
+    } // body
+} // LoginView
 
 #Preview {
     LoginView()
--- FlashBond/Authentication/ViewModel/LoginViewModel.swift
+++ FlashBond/Authentication/ViewModel/LoginViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class LoginViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+    
+}
