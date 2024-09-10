dece2d39c69568ca3c22c2af770a6a68e2e87d15
Wataru Takahashi <wataru1355@icloud.com>
2024-09-11 01:02:20 +0900

    fix: AuthServiceのプロパティの変更がContentviewModelのプロパティと同期するように処理を追加

--- FlashBond/Root/ContentViewModel.swift
+++ FlashBond/Root/ContentViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import FirebaseAuth
+import Combine
+
+class ContentViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
+
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+
+    init() {
+        setupUserSessionSubscriber()
+        setupCurrentUserSubscriber()
+    }
+
+    // userSessionに関する購読設定
+    private func setupUserSessionSubscriber() {
+        service.$userSession
+            .sink { [weak self] userSession in
+                self?.userSession = userSession
+            }
+            .store(in: &cancellables)
+    }
+
+    // currentUserに関する購読設定
+    private func setupCurrentUserSubscriber() {
+        service.$currentUser
+            .sink { [weak self] currentUser in
+                self?.currentUser = currentUser
+            }
+            .store(in: &cancellables)
+    }
+
+    func forceSignout() {
+        // service.signout()
+        print("ユーザデータが見つかりませんでした")
+    }
+}
