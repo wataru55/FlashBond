c3d552cea2182234114f4c6576b4b603fd373f0d
Wataru Takahashi <wataru1355@icloud.com>
2024-09-10 17:20:02 +0900

    add: 新規ファイルの追加

---
+++ FlashBond/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  FlashBond
+//
+//  Created by  髙橋和 on 2024/09/10.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
