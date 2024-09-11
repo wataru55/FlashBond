1491d2191caaf8e3226a61152279269b0a3e3c01
Wataru Takahashi <wataru1355@icloud.com>
2024-09-11 21:48:45 +0900

    add: 新規ファイルの追加

---
+++ FlashBond/Views/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  FlashBond
+//
+//  Created by  髙橋和 on 2024/09/11.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
