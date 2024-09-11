2ae30e1d07ad0ece319beec6d6bb1fd8ac99c8f1
Wataru Takahashi <wataru1355@icloud.com>
2024-09-11 17:31:58 +0900

    add: 新規ファイルの追加

---
+++ FlashBond/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  FlashBond
+//
+//  Created by  髙橋和 on 2024/09/11.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
