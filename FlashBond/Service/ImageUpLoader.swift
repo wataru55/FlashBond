435902c1b431d052fa86a04548a661283214ff44
Wataru Takahashi <wataru1355@icloud.com>
2024-09-14 19:34:36 +0900

    fix: Firebase Storageに画像データを保存してその画像URLを取得するメソッドを作成

--- FlashBond/Service/ImageUpLoader.swift
+++ FlashBond/Service/ImageUpLoader.swift
@@ -10,21 +10,27 @@ import Firebase
 import FirebaseStorage
 
 struct ImageUpLoader {
-    static func uploadImage(image: UIImage) async throws -> String? {
+    /// 画像データをFirebase Storageに保存してそのダウンロードURLを返す関数
+    static func uploadImageToStorage(image: UIImage) async throws -> String? {
+        // uiImageをJPEG形式のバイナリデータに変換
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return nil }
-        guard let uid = AuthService.shared.currentUser?.id else { return nil}
+        // ファイル名としてUUIDを生成
         let filename = NSUUID().uuidString
-        let ref = Storage.storage().reference(withPath: "/profile_images/\(uid)/\(filename)")
+        // Firebase Storageの参照を作成し、保存先のパスを指定
+        let storageRef = Storage.storage().reference(withPath: "/profile_images/\(filename)")
 
-        // コンテンツタイプを設定
+        // Firebase Storageに保存する際のメタデータを設定
         let metadata = StorageMetadata()
         metadata.contentType = "image/jpeg"
 
         do {
-            let _ = try await ref.putDataAsync(imageData, metadata: metadata)
-            let url = try await ref.downloadURL()
+            // 画像データをStorageに保存する関数
+            let _ = try await storageRef.putDataAsync(imageData, metadata: metadata)
+            // 保存が成功したらダウンロードURLを取得して，文字列を返す
+            let url = try await storageRef.downloadURL()
             return url.absoluteString
         } catch {
+            // エラーが発生した場合、エラーメッセージを出力してnilを返す
             print("DEBUG: Failed to upload image with error \(error.localizedDescription)")
             return nil
         }
