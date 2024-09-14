1d4d2849f7221180369d8a9041be42781df08b75
Wataru Takahashi <wataru1355@icloud.com>
2024-09-14 11:19:09 +0900

    fix: プロフィール画像，ユーザーネーム，詳細文を設定するビューを作成

--- FlashBond/Views/EditProfileView.swift
+++ FlashBond/Views/EditProfileView.swift
@@ -8,8 +8,129 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    @StateObject private var viewModel = EditProfileViewModel()
+    @Environment(\.dismiss) var dismiss
+
+    @State private var isPickerPresented = false
+    @State private var isEditing = false
+    @State var textfield: String = ""
+
+    let _width =  UIScreen.main.bounds.width
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            ScrollView(.vertical, showsIndicators: false){
+                // 画像
+                // TODO: 保存した画像を表示する
+                if let displayImage = viewModel.displayImage {
+                    displayImage
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: _width - 10, height: UIScreen.main.bounds.height / 2)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .clipped()
+                        .onTapGesture {
+                            if isEditing {
+                                isPickerPresented.toggle()
+                            }
+                        }
+                } else {
+                    Button {
+                        isPickerPresented.toggle()
+                    } label: {
+                        RoundedRectangle(cornerRadius: 12).stroke(style: .init(lineWidth: 2, dash: [2, 4]))
+                            .foregroundStyle(.black)
+                            .padding(.horizontal, 5)
+                            .frame(width: _width, height: UIScreen.main.bounds.height / 2)
+                            .overlay {
+                                VStack {
+                                    Image(systemName: "photo")
+                                        .font(.title)
+                                        .foregroundStyle(.black)
+                                        .padding(.bottom, 10)
+
+                                    Text("画像を選択")
+                                        .font(.subheadline)
+                                        .foregroundStyle(.gray)
+                                }
+                            }
+                    }
+                    .padding(.bottom, 10)
+                }
+
+                Divider()
+                    .padding(.bottom, 10)
+
+                // ユーザーネーム
+                // TODO: ユーザーネームをデータベースから取得する
+                Section {
+                    TextField("ユーザーネーム", text: $viewModel.username)
+                        .frame(width: _width)
+                        .modifier(TextFieldModifier())
+                        .allowsHitTesting(isEditing)
+                        .padding(.leading, 10)
+
+                } header: {
+                    Text("ユーザーネーム")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .frame(width: _width, alignment: .leading)
+                        .padding(.leading, 10)
+                }
+
+                // ユーザーについての詳細文
+                // TODO: 詳細文をデータベースから取得する
+                Section {
+                    TextField("詳細文", text: $viewModel.bio, axis: .vertical)
+                        .frame(width: _width, height: 150)
+                        .modifier(TextFieldModifier())
+                        .lineLimit(0...10)
+                        .allowsHitTesting(isEditing)
+                        .padding(.leading, 10)
+
+                } header: {
+                    Text("詳細文")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .frame(width: _width, alignment: .leading)
+                        .padding(.leading, 10)
+                        .padding(.top, 10)
+                }
+            }// scrollView
+            .photosPicker(isPresented: $isPickerPresented, selection: $viewModel.selectedImage, matching: .images)
+        }// VStack
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .font(.footnote)
+                        .foregroundStyle(.black)
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                HStack {
+                    Button {
+                        isEditing.toggle()
+                    } label: {
+                        Text("編集")
+                            .font(.subheadline)
+                            .foregroundStyle(isEditing ? .gray : .black)
+                    }
+
+                    Button {
+
+                    } label: {
+                        Text("保存")
+                            .font(.subheadline)
+                            .foregroundStyle(isEditing ? .black : .gray)
+                            .disabled(isEditing ? false : true)
+                    }
+                }
+            }
+        }
     }
 }
 
