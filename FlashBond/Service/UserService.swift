4f378f2a3c7583236bf4df0544c8f9ac2647886f
Wataru Takahashi <wataru1355@icloud.com>
2024-09-15 10:52:02 +0900

    fix: ユーザーデータをフェッチするメソッドを実装

--- FlashBond/Service/UserService.swift
+++ FlashBond/Service/UserService.swift
@@ -5,4 +5,14 @@
 //  Created by  髙橋和 on 2024/09/15.
 //
 
-import Foundation
+import SwiftUI
+import Firebase
+
+struct UserService {
+    static func fetchUserData(uid: String) async throws -> User {
+        // ユーザidを使用してFirestoreDatabaseからドキュメントを取得
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        // snapshotからUser型にデータをデコードして値を返す
+        return try snapshot.data(as: User.self)
+    }
+}
