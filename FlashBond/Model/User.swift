343135f3e4c1b60d804d0fd65e6d4dbb01f767eb
Wataru Takahashi <wataru1355@icloud.com>
2024-09-15 12:10:26 +0900

    fix: データ構造と一致するプロパティ名に変更

--- FlashBond/Model/User.swift
+++ FlashBond/Model/User.swift
@@ -11,12 +11,13 @@ struct User: Identifiable, Hashable, Codable {
     let id: String
     var username: String
     var email: String
-    var profileImageUrl: String?
+    var imageURL: String?
+    var bio: String?
 }
 
 extension User {
     static var MOCK_USERS: [User] = [
-        .init(id: NSUUID().uuidString, username: "ironman", email: "ironman@gmail.com", profileImageUrl: "ironman1"),
+        .init(id: NSUUID().uuidString, username: "ironman", email: "ironman@gmail.com", imageURL: "ironman1"),
     ]
 }
 
